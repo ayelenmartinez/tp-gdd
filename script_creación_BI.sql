767fb28a982240dcc1d67b0514e12136fff5eb41
ayelenmartinez <49215533+ayelenmartinez@users.noreply.github.com>
2024-12-04 01:21:18 -0300

    drops preventivos y create hecho venta

--- script_creación_BI.sql
+++ script_creación_BI.sql
@@ -3,6 +3,22 @@ GO
 
 
 ------------------------------------------------------------ ELIMINACION PREVENTIVA DE TABLAS ------------------------------------------------------------
+EXEC sp_MSforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT ALL";
+
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Hecho_publicacion', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Hecho_publicacion;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Hecho_Envio', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Hecho_Envio;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Hecho_Venta', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Hecho_Venta;
+
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Marca', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Marca;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Rubro_Subrubro', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Rubro_Subrubro;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Tipo_medio_pago', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Tipo_medio_pago;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Tipo_envio', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Tipo_envio;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Rango_horario', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Rango_horario;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Rango_etario', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Rango_etario;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Ubicacion', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Ubicacion;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Tiempo', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Tiempo;
+
+EXEC sp_MSforeachtable "ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL";
 
 ------------------------------------------------------------ FIN DE ELIMINACION PREVENTIVA DE TABLAS ------------------------------------------------------------
 
@@ -40,13 +56,13 @@ CREATE TABLE LOS_ANTI_PALA.BI_Tipo_Envio (
 
 	CREATE TABLE LOS_ANTI_PALA.BI_Tipo_Medio_Pago (
 	tipo_medio_pago_codigo BIGINT PRIMARY KEY NOT NULL,
-	tipo_medio_pago NVARCHAR(100),
+	tipo_medio_pago NVARCHAR(50),
 );
 
 CREATE TABLE LOS_ANTI_PALA.BI_Rubro_Subrubro (
 	rubro_subrubro_codigo BIGINT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	rubro NVARCHAR(100),
-	subrubro NVARCHAR(100),
+	rubro NVARCHAR(50),
+	subrubro NVARCHAR(50),
 );
 
 CREATE TABLE LOS_ANTI_PALA.BI_Marca (
@@ -54,7 +70,16 @@ CREATE TABLE LOS_ANTI_PALA.BI_Marca (
     marca_descripcion NVARCHAR(50),
 );
 
----------- Hechos ----------
+---------- Tablas de hecho ----------
+
+CREATE TABLE LOS_ANTI_PALA.BI_Hecho_Venta (
+	tiempo_codigo BIGINT REFERENCES LOS_ANTI_PALA.BI_Tiempo NOT NULL,
+	ubicacion_codigo BIGINT REFERENCES LOS_ANTI_PALA.BI_Ubicacion NOT NULL,
+	rubro_subrubro_codigo BIGINT REFERENCES LOS_ANTI_PALA.BI_Rubro_subrubro NOT NULL,
+	rango_etario_codigo BIGINT REFERENCES LOS_ANTI_PALA.BI_Rango_etario NOT NULL,
+	rango_horario_codigo BIGINT REFERENCES LOS_ANTI_PALA.BI_Rango_horario NOT NULL,
+	tipo_medio_pago_codigo BIGINT REFERENCES LOS_ANTI_PALA.BI_Tipo_medio_pago NOT NULL,
+);
 
 CREATE TABLE LOS_ANTI_PALA.BI_Hecho_Publicacion (
 	codigo_tiempo BIGINT REFERENCES LOS_ANTI_PALA.BI_Tiempo NOT NULL,
@@ -68,4 +93,13 @@ CREATE TABLE LOS_ANTI_PALA.BI_Hecho_Envio (
 	tipo_envio_codigo BIGINT REFERENCES LOS_ANTI_PALA.BI_Tipo_envio NOT NULL,
 
 )
+
+
 ------------------------------------------------------------ FIN CREACION DE TABLAS ------------------------------------------------------------
+
+
+
+
+
+------------------------------------------------------------ MIGRACION DE TABLAS ------------------------------------------------------------
+
