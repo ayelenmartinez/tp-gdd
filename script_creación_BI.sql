1561f8f92585aa399df0b088e2d6591f41d646e1
StephieOrtiz <angelastefi97@gmail.com>
2024-12-04 20:18:43 -0300

    Migracion tiempo y ubicacion

--- script_creación_BI.sql
+++ script_creación_BI.sql
@@ -5,10 +5,10 @@ GO
 ------------------------------------------------------------ ELIMINACION PREVENTIVA DE TABLAS ------------------------------------------------------------
 EXEC sp_MSforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT ALL";
 
-IF OBJECT_ID('LOS_ANTI_PALA.BI_Hecho_publicacion', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Hecho_publicacion;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Hecho_Publicacion', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Hecho_Publicacion;
 IF OBJECT_ID('LOS_ANTI_PALA.BI_Hecho_Envio', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Hecho_Envio;
 IF OBJECT_ID('LOS_ANTI_PALA.BI_Hecho_Venta', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Hecho_Venta;
-IF OBJECT_ID('LOS_ANTI_PALA.BI_Hecho_Venta', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Hecho_Facturacion;
+IF OBJECT_ID('LOS_ANTI_PALA.BI_Hecho_Facturacion', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Hecho_Facturacion;
 
 IF OBJECT_ID('LOS_ANTI_PALA.BI_Marca', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Marca;
 IF OBJECT_ID('LOS_ANTI_PALA.BI_Rubro_Subrubro', 'U') IS NOT NULL DROP TABLE LOS_ANTI_PALA.BI_Rubro_Subrubro;
@@ -93,7 +93,7 @@ CREATE TABLE LOS_ANTI_PALA.BI_Hecho_Envio (
 	ubicacion_codigo BIGINT REFERENCES LOS_ANTI_PALA.BI_Ubicacion NOT NULL,
 	tipo_envio_codigo BIGINT REFERENCES LOS_ANTI_PALA.BI_Tipo_envio NOT NULL,
 
-)
+);
 
 CREATE TABLE LOS_ANTI_PALA.BI_Hecho_Facturacion (
     tiempo_codigo BIGINT REFERENCES LOS_ANTI_PALA.BI_Tiempo NOT NULL,
@@ -205,6 +205,56 @@ BEGIN
 END
 GO
 
+IF OBJECT_ID('migrar_tabla_bi_ubicacion', 'P') IS NOT NULL
+	DROP PROCEDURE migrar_tabla_bi_ubicacion;
+GO
+CREATE PROCEDURE migrar_tabla_bi_ubicacion
+AS
+BEGIN
+	INSERT INTO LOS_ANTI_PALA.BI_Ubicacion(ubicacion_provincia, ubicacion_localidad)
+	SELECT DISTINCT 
+		d.domicilio_provincia,
+		d.domicilio_localidad
+		FROM LOS_ANTI_PALA.Domicilio d
+
+	PRINT ('Tabla "Ubicacion" del BI migrada')
+END
+GO
+
+IF OBJECT_ID ('migrar_tabla_bi_tiempo', 'P') IS NOT NULL
+	DROP PROCEDURE migrar_tabla_bi_tiempo;
+GO
+CREATE PROCEDURE migrar_tabla_bi_tiempo
+AS
+BEGIN
+	INSERT INTO LOS_ANTI_PALA.BI_Tiempo(tiempo_anio,tiempo_cuatrimestre,tiempo_mes)
+	SELECT DISTINCT
+	YEAR(v.venta_fecha), AS ANIO
+	LOS_ANTI_PALA.getCuatrimestre(v.venta_fecha), AS CUATRIMESTRE
+	MONTH(v.venta_fecha) AS MES
+	FROM LOS_ANTI_PALA.Venta v
+	UNION 
+	SELECT DISTINCT 
+	YEAR(f.factura_fecha), AS ANIO
+	LOS_ANTI_PALA.getCuatrimestre(f.factura_fecha), AS CUATRIMESTRE
+	MONTH(f.factura_fecha) AS MES
+	FROM LOS_ANTI_PALA.Factura f
+	UNION
+	SELECT DISTINCT
+	YEAR(p.pago_fecha),AS ANIO
+	LOS_ANTI_PALA.getCuatrimestre(p.pago_fecha), AS CUATRIMESTRE
+	MONTH(p.pago_fecha) AS MES
+	FROM LOS_ANTI_PALA.Pago P
+
+	--FALTA TERMINAR
+
+
+PRINT ('Tabla "Tiempo" del BI migrada')
+END
+GO
+
+
+
 ------------------------------------------------------------ FIN MIGRACION DE TABLAS ------------------------------------------------------------
 
 GO
@@ -215,7 +265,9 @@ EXEC migrar_tabla_bi_marca;
 EXEC migrar_tabla_bi_rango_etario;
 EXEC migrar_tabla_bi_rango_horario;
 EXEC migrar_tabla_bi_tipo_envio;
+EXEC migrar_tabla_bi_ubicacion;
 EXEC migrar_tabla_bi_tipo_pago;
+EXEC migrar_tabla_bi_tiempo;
 
 	PRINT '--- Todas las tablas del BI fueron migradas correctamente --';
 COMMIT TRANSACTION
@@ -224,3 +276,31 @@ BEGIN CATCH
 ROLLBACK TRANSACTION;
 		THROW 50001, 'No se migraron correctamente las tablas del BI', 1;
 END CATCH
+
+
+
+---- funciones auxiliares----
+
+IF OBJECT_ID('[EXEL_ENTES].getCuatrimestre', 'FN') IS NOT NULL
+BEGIN
+    DROP FUNCTION [EXEL_ENTES].getCuatrimestre;
+END
+
+GO
+
+
+alter FUNCTION LOS_ANTI_PALA.getCuatrimestre (@fecha DATE)
+RETURNS SMALLINT
+AS BEGIN
+DECLARE @nro_cuatrimestre SMALLINT
+SET @nro_cuatrimestre =
+	CASE
+		WHEN MONTH(@fecha) BETWEEN 1 AND 4 THEN 1
+		WHEN MONTH(@fecha) BETWEEN 5 AND 8 THEN 2
+		WHEN MONTH(@fecha) BETWEEN 9 AND 12 THEN 3
+	END
+
+RETURN @nro_cuatrimestre
+
+END
+GO
